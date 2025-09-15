99a5efe41f323a5e2546fa9f9520d209d9e7475c
lscyane <lscyane@gmail.com>
2025-09-15 13:59:40 +0900

    ダイアログの最低限のClose要求処理対応

--- lscyane.Core/Mvvm/DialogViewModelBase.cs
+++ lscyane.Core/Mvvm/DialogViewModelBase.cs
@@ -23,5 +23,21 @@ namespace lscyane.Core.Mvvm
         /// ダイアログ表示前に呼び出される
         /// </summary>
         public abstract void OnDialogPreviewOpen(DialogParameters? parameter);
+
+
+        /// <summary>
+        /// Close要求イベント
+        /// </summary>
+        /// <remarks> <see cref="lscyane.Wpf.Services.DialogService.GetDialogViewWithPreProcess"/> で設定 </remarks>
+        public event System.Action? RequestClose;
+
+
+        /// <summary>
+        /// Close要求
+        /// </summary>
+        protected virtual void OnRequestClose()
+        {
+            this.RequestClose?.Invoke();
+        }
     }
 }
