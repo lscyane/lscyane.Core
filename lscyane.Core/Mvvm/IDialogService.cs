315dded7dc787da3d8d858ce210eb80ea974197a
lscyane <lscyane@gmail.com>
2025-09-15 11:22:12 +0900

    モーダルダイアログの対応

--- lscyane.Core/Mvvm/IDialogService.cs
+++ lscyane.Core/Mvvm/IDialogService.cs
@@ -7,6 +7,22 @@ namespace lscyane.Core.Mvvm
     /// </summary>
     public interface IDialogService
     {
+        /// <summary>
+        /// ダイアログをモードレスで表示します
+        /// </summary>
+        /// <param name="vm_type"></param>
+        /// <param name="parameter"></param>
+        /// <param name="result_action"></param>
         public void Show(Type vm_type, DialogParameters? parameter = null, Action<object>? result_action = null);
+
+
+        /// <summary>
+        /// ダイアログをモーダルで表示します
+        /// </summary>
+        /// <param name="vm_type"></param>
+        /// <param name="parameter"></param>
+        /// <param name="result_action"></param>
+        public void ShowDialog(Type vm_type, DialogParameters? parameter = null, Action<object>? result_action = null);
+        
     }
 }
